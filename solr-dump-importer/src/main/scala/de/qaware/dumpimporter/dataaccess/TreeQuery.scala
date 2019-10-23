a07bb7989078572a16d2940f814e8cb81cdd25f9
Fabian Huch <fabian.huch@qaware.de>
2019-10-23 14:38:41 +0200

    Added Reducible higher kinded type

--- solr-dump-importer/src/main/scala/de/qaware/dumpimporter/dataaccess/TreeQuery.scala
+++ solr-dump-importer/src/main/scala/de/qaware/dumpimporter/dataaccess/TreeQuery.scala
@@ -1,19 +1,109 @@
 package de.qaware.dumpimporter.dataaccess
 
 import scala.collection.mutable.ListBuffer
+import scala.language.{higherKinds, implicitConversions}
+
+import de.qaware.dumpimporter.dataaccess
+import org.apache.commons.lang3.NotImplementedException
+
+trait Reducible[+A, M[_] <: Reducible[_, M]] extends Any {
+  def apply[B](elems: Iterable[M[B]]): M[B]
+  def flatMap[B](f: A => M[B]): M[B]
+}
+class IterableReducible[A](val inner: Iterable[A]) extends AnyVal with Reducible[A, IterableReducible] {
+  override def apply[B](elems: Iterable[IterableReducible[B]]): IterableReducible[B] = new IterableReducible(elems).flatMap(identity)
+  override def flatMap[B](f: A => IterableReducible[B]): IterableReducible[B] = new IterableReducible(inner.flatMap(e => f(e).inner))
+}
+object IterableReducible {
+  implicit def toInner[A](outer: IterableReducible[A]): Iterable[A] = outer.inner
+}
+class SingletonReducible[A](val inner: A) extends AnyVal with Reducible[A, SingletonReducible] {
+  override def apply[B](elems: Iterable[SingletonReducible[B]]): SingletonReducible[B] = elems.toSeq match {
+    case Seq(singleton) => singleton
+    case iterable => throw new IllegalArgumentException(s"Tried to build singleton reducible on non-singleton iterable $iterable")
+  }
+  override def flatMap[B](f: A => SingletonReducible[B]): SingletonReducible[B] = f(inner)
+}
+object SingletonReducible {
+  implicit def toInner[A](outer: SingletonReducible[A]): A = outer.inner
+}
 
 /** Generic immutable tree node.
   *
   * @tparam A type of inner element
   * @tparam T type of whole node
   */
-trait Node[+A, T <: Node[A, T]] { self: T =>
-
+trait Node[+A, T <: Node[A, T]] {
   /** Inner element. */
   val data: A
 
   /** Children of this tree node. */
   val children: Seq[T]
+}
+
+/** Value class for the lengthier node to partial selection function. */
+//class ChildFilter[N <: Node[_, N]](val inner: N => PartialFunction[N, Boolean]) extends AnyVal
+sealed trait Connective
+case object of extends Connective
+case object ofOne extends Connective
+
+final case class FilterQuery[N <: Node[_, N]](result: N => Boolean, recursion: N => N => Boolean = (_: N) => (_: N) => true) {
+  def and(other: FilterQuery[N]): FilterQuery[N] = {
+    FilterQuery((n: N) => result(n) && other.result(n), (n: N) => (c: N) => recursion(n)(c) && other.recursion(n)(c))
+  }
+  def without(other: FilterQuery[N]): FilterQuery[N] = {
+    FilterQuery((n: N) => result(n) && !other.result(n), recursion)
+  }
+}
+object FilterQuery {
+  def not[N <: Node[_, N]](fq: FilterQuery[N]): FilterQuery[N] = {
+    FilterQuery(!fq.result(_))
+  }
+  def property[N <: Node[_, N]](filter: N => Boolean): FilterQuery[N] = {
+    FilterQuery(filter)
+  }
+}
+
+final case class ResultTransformQuery[N <: Node[_, N], R[_] <: Reducible[_, R]](transform: N => N => Iterable[N])(implicit ev: R[N]) {
+  def in(forest: Iterable[N]): R[N] = { // TODO root node
+    val resultIterable = for {
+      node <- forest
+      child <- node.children
+    } yield transform(node)(child)
+    ev(resultIterable)
+  }
+
+  def in(tree: N): R[N] = in(Seq(tree))
+
+  def thats(filter: FilterQuery[N]): ResultTransformQuery[N, R] = {
+    ResultTransformQuery { n: N => c: N =>
+      if (filter.recursion(n)(c)) {
+        transform(n)(c).filter(filter.result)
+      } else {
+        Seq()
+      }
+    }
+  }
+
+  def first(of: Connective): ResultTransformQuery[N, R] = ???
+  def root(of: Connective): ResultTransformQuery[N, R] = ???
+  def previous(of: Connective): ResultTransformQuery[N, R] = ???
+  def next(of: Connective): ResultTransformQuery[N, R] = ???
+  def parent(of: Connective): ResultTransformQuery[N, R] = ???
+}
+object ResultTransformQuery {
+  def findRec[N <: Node[_, N]](node: N, child: N): Iterable[N] = {
+    child.children.flatMap(grandChild => findRec(child, grandChild))
+  }
+  private def getQuery[N <: Node[_, N], R[_] <: Reducible[_, R]](implicit ev: R[N]) = {
+    ResultTransformQuery((node: N) => (child: N) => findRec(node, child))(ev)
+  }
+  def single[N <: Node[_, N]](implicit ev: N): ResultTransformQuery[N, SingletonReducible] = {
+    getQuery(new SingletonReducible[N](ev))
+  }
+  def all[N <: Node[_, N]](implicit ev: N): ResultTransformQuery[N, IterableReducible] = {
+    getQuery(new IterableReducible[N](Seq(ev)))
+  }
 }
 
 /** Functional representation of a query to a tree consisting of [[Node]]s.
