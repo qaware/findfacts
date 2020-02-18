baabde13aac1c6c213df562f5db1ef14f2661e58
Fabian Huch <fabian.huch@qaware.de>
2020-02-18 17:15:05 +0100

    Fixed test

--- common-da-solr/src/test/scala/de/qaware/findfacts/common/solr/LocalSolrTest.scala
+++ common-da-solr/src/test/scala/de/qaware/findfacts/common/solr/LocalSolrTest.scala
@@ -8,20 +8,6 @@ import org.scalatest.{FunSuite, Matchers}
 
 class LocalSolrTest extends FunSuite with Matchers {
 
-  test("Test uniqueness") {
-    val solrRepo = LocalSolr(new File(Resource.getUrl("solr/").getFile), "testdata")
-    val conn1 = solrRepo.solrConnection().asInstanceOf[EmbeddedSolrServer]
-    val conn2 = solrRepo.solrConnection().asInstanceOf[EmbeddedSolrServer]
-
-    val dir1 = conn1.getCoreContainer.getCoreRootDirectory
-    val dir2 = conn2.getCoreContainer.getCoreRootDirectory
-
-    dir1 should not equal dir2
-
-    conn1.close()
-    conn2.close()
-  }
-
   test("Test without config") {
     assertThrows[IllegalArgumentException] { LocalSolr(new File(""), "missing") }
   }
