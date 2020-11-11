4567f3cfdd73e72d2638250161f77c175d80a465
Fabian Huch <huch@in.tum.de>
2020-11-11 18:50:20 +0100

    Allowed cross-origin requests

--- search-webapp/app/de/qaware/findfacts/webapp/WebappLoader.scala
+++ search-webapp/app/de/qaware/findfacts/webapp/WebappLoader.scala
@@ -12,6 +12,7 @@ import play.api.mvc.EssentialFilter
 import play.api.routing.Router
 import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext, ConfigLoader}
 import play.filters.HttpFiltersComponents
+import play.filters.cors.CORSComponents
 import play.filters.csrf.CSRFFilter
 import play.filters.gzip.GzipFilterComponents
 import play.modules.swagger.{SwaggerPlugin, SwaggerPluginImpl}
@@ -39,13 +40,14 @@ class WebappModule(context: Context)
   with SolrQueryModule
   with AssetsComponents
   with HttpFiltersComponents
+  with CORSComponents
   with GzipFilterComponents {
 
   lazy val statsLoggingFilter: StatsLoggingFilter = wire[StatsLoggingFilter]
 
   // Disable CSRF, as it is not needed in this application, and add gzip filter
   override def httpFilters: Seq[EssentialFilter] = {
-    super.httpFilters.filterNot(_.getClass == classOf[CSRFFilter]) :+ gzipFilter :+ statsLoggingFilter
+    super.httpFilters.filterNot(_.getClass == classOf[CSRFFilter]) :+ gzipFilter :+ statsLoggingFilter :+ corsFilter
   }
 
   // Connect to remote solr.
